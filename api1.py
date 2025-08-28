be3c9b5ef7d2f09b4b7dab122390d7f4ac7620db
Salome-gil <salomechanci@gmail.com>
2025-08-27 19:46:22 -0500

    cambios

--- api1.py
+++
@@ -1,7 +0,0 @@
-def Resta(num1:int, num2:int)-> int:
-    Respuesta= num1 - num2
-    return Respuesta
-
-Resultado= Resta(1,2)
-print(Resultado)
-    
--- main1.py
+++
@@ -1,7 +0,0 @@
-def Sumar(num1:int, num2:int)-> int:
-    Respuesta= num1 + num2
-    return Respuesta
-
-Resultado= Sumar(1,2)
-print(Resultado)
-    
