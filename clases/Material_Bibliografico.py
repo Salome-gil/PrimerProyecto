90e92e353b00fc978666ed846bf510adcb194791
DESKTOP-7ST3MPC\Janus <mafe.palacio53@gmail.com>
2025-08-28 21:29:48 -0500

    fix: ajustes en clase Material con los metodos completos y los que faltaban

--- clases/Material_Bibliografico.py
+++ clases/Material_Bibliografico.py
@@ -39,6 +39,26 @@ class Material_Bibliografico:
         self.__reservado= reservado
 
 #------------------------------------------------------------------------------------------
+#  toString()
+#  Retorna una representación en cadena de texto con la información principal del material.
+#-----------------------------------------------------------------------------------------    
+
+    def __str__(self) -> str:
+        return f"Código: {self.__Id}, Estado: {self.__estado}, Reservado: {self.__reservado}"
+
+#------------------------------------------------------------------------------------------
+#  METODOS DE ESTADO
+#-----------------------------------------------------------------------------------------    
+
+    # Cambia el estado del material a "disponible".
+    def marcar_disponible(self) -> None:
+        self.__estado = "disponible"
+    
+    # Cambia el estado del material a "prestado".
+    def marcar_no_disponible(self) -> None:
+        self.__estado = "prestado"
+
+#------------------------------------------------------------------------------------------
 #  RESERVAR MATERIAL
 #-----------------------------------------------------------------------------------------    
     
@@ -46,6 +66,9 @@ class Material_Bibliografico:
     def reservar(self, cod_cliente: int) -> bool:
         if self.__estado == "disponible":  # Solo se permite reservar el material si está disponible
             if not self.__reservado:
+                self.__reservado= True  # Cambia los estados para que no se reserve el mismo material varias veces
+                self.__estado= "reservado"
+                self.__cliente_reserva = cod_cliente  # cliente que desea reservar el material.
                 return True  # si la reserva se realizó correctamente.
             else:
                 return False 
@@ -59,6 +82,9 @@ class Material_Bibliografico:
     # Cancela la reserva si el cliente que la hizo coincide con el que intenta cancelarla.
     def cancelar_reserva(self, cod_cliente: int) -> bool:
         if self.__reservado and self.__cliente_reserva == cod_cliente:  # Compara el cod del cliente que hizo la reserva con el que intenta cancelarla
+            self.__reservado= False  
+            self.__estado= "disponible"  # Cambia los estados para que se vuelva a estar disponible
+            self.__cliente_reserva = None
             return True  # si la reserva fue cancelada exitosamente.
         elif self.__reservado:
             return False  # si la reserva no puede ser cancelada (otro cliente la reservó).
